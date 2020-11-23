6997e9e2e27b41d2e3437c9a9274e53555a6e63d
Shaun Lobsinger <shaun@jolobo.com>
2020-11-22 22:03:06 -0700

    added some animation

--- src/react/components/Hero.jsx
+++ src/react/components/Hero.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './Hero.scss';
 
+// 
+
 const Hero = () => (
   <div className="hero-section">
     <div className="container hero-section__container">
-      <p className="hero-section__sub-title">In loving memory of</p>
-      <p className="hero-section__title">Tylie Sanders</p>
-      <p className="hero-section__sub-title">1985 - 2020</p>
+      <p className="hero-section__sub-title animate__animated animate__fadeIn animate__delay-1s">In loving memory of</p>
+      <p className="hero-section__title animate__animated animate__fadeIn animate__delay-2s">Tylie Sanders</p>
+      <p className="hero-section__sub-title animate__animated animate__fadeIn animate__delay-2s">1985 - 2020</p>
     </div>
   </div>
 
