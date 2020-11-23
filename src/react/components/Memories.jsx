5853f5866893ee987a732114dd2836c4c30b5b20
Shaun Lobsinger <shaun@jolobo.com>
2020-11-22 21:48:23 -0700

    eslint fixes, added memories form

--- src/react/components/Memories.jsx
+++ src/react/components/Memories.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button, Modal } from 'react-bootstrap';
 
 import Section from './Section';
 
-const Memories = () => (
-    <Section title="Memories">
+const { Group, Label, Control } = Form;
+const { Header, Title, Body } = Modal;
 
-    </Section>
-);
+const MemoryFormModal = ({ show, onDismiss }) => (
+<Modal show={show} onHide={onDismiss} size="lg">
+  <Header closeButton>
+    <Title>Add Your Memory</Title>
+  </Header>
+  <Body>
+    <Form>
+        <Group controlId="formBasicEmail">
+            <Label>Your First Name</Label>
+            <Control type="text" placeholder="James" />
+        </Group>
+        <Group controlId="formBasicEmail">
+            <Label>Your Memory</Label>
+            <Control as="textarea" rows={3} />
+        </Group>
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+    </Form>
+  </Body>
+
+</Modal>
+)
+
+const Memories = () => {
+    const [ showForm, setShowForm ] = useState(false);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        setShowForm(true);
+    }
+
+    const handleDismiss = () => {
+        setShowForm(false);
+    }
+
+    return (
+        <Section title="Memories">
+            <a href="#" onClick={handleClick}>Add a memory</a>
+            <MemoryFormModal onDismiss={handleDismiss} show={showForm} />
+        </Section>
+    );
+};
 
 export default Memories;
--- src/react/components/Navigation.jsx
+++ src/react/components/Navigation.jsx
@@ -11,6 +11,7 @@ const Navigation = () => (
   <Toggle aria-controls="basic-navbar-nav" />
   <Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
+        <Link href="#link">Eulogy</Link>
       <Link href="#link">Photos</Link>
       <Link href="#link">Memories</Link>
     </Nav>
