332bf5a1e6baea34e85414e3ca932ed688cb158d
Shaun Lobsinger <shaun@jolobo.com>
2020-11-26 23:51:20 -0700

    added additional UI tweaks.

--- src/react/components/Memories.jsx
+++ src/react/components/Memories.jsx
@@ -1,36 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Form, Button, Modal } from 'react-bootstrap';
+// import ReCAPTCHA from 'react-google-recaptcha';
 import PropTypes from 'prop-types';
+import Swal from 'sweetalert2';
 
 import Section from './Section';
+
+import './Memories.scss';
 
 const { Group, Label, Control } = Form;
 const { Header, Title, Body } = Modal;
 
-const MemoryFormModal = ({ show, onDismiss }) => (
-  <Modal show={show} onHide={onDismiss} size="lg">
-    <Header closeButton>
-      <Title>Add Your Memory</Title>
-    </Header>
-    <Body>
-      <Form>
-        <Group>
-          <Label>Your First Name</Label>
-          <Control type="text" placeholder="James" />
-        </Group>
-        <Group>
-          <Label>Your Memory</Label>
-          <Control as="textarea" maxLength={500} rows={3} />
-        </Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Body>
+const INITIAL_FORM_STATE = { name: '', comment: '' };
 
-  </Modal>
-);
+const MemoryFormModal = ({ show, onDismiss, onSuccess }) => {
+  // const captcha = useRef();
+  const [formState, setFormState] = useState(INITIAL_FORM_STATE);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    const body = {
+      ...formState,
+      // captcha: captcha.current.getValue(),
+    };
+
+    axios.post('/.netlify/functions/newcomment', body).then(() => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Your memory has been added.',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+
+      setFormState(INITIAL_FORM_STATE);
+      setLoading(false);
+      onDismiss();
+      onSuccess();
+    })
+      .catch(() => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Something went wrong, please try again.',
+        });
+        setLoading(false);
+      });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  return (
+    <Modal show={show} onHide={onDismiss} size="lg">
+      <Header closeButton>
+        <Title>Add Your Memory</Title>
+      </Header>
+      <Body>
+        <Form onSubmit={handleSubmit}>
+          <Group>
+            <Label>Your First Name</Label>
+            <Control value={formState.name} onChange={handleChange} name="name" type="text" placeholder="James" />
+          </Group>
+          <Group>
+            <Label>Your Memory</Label>
+            <Control value={formState.comment} onChange={handleChange} name="comment" as="textarea" maxLength={500} rows={3} />
+          </Group>
+          {/* <ReCAPTCHA ref={captcha} sitekey="6LdzKe8ZAAAAACCavBmL9LWfAaAG7fvoHoIVYYB5" /> */}
+          <br />
+          <Button disabled={loading} variant="primary" type="submit">
+            {loading ? 'Submitting...' : 'Submit'}
+          </Button>
+        </Form>
+      </Body>
+    </Modal>
+  );
+};
 
 MemoryFormModal.propTypes = {
   show: PropTypes.bool.isRequired,
@@ -41,9 +94,11 @@ const Memories = () => {
   const [showForm, setShowForm] = useState(false);
   const [comments, setComments] = useState([]);
 
+  const fetchComments = () => axios.get('/.netlify/functions/comment').then((res) => setComments(res.data.data));
+
   useEffect(() => {
-    axios.get('/.netlify/functions/comment').then((res) => setComments(res.data.data));
-  }, []);
+    fetchComments();
+  }, [fetchComments]);
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -56,9 +111,10 @@ const Memories = () => {
   };
 
   return (
-    <Section title="Memories" id="memories">
+    <Section title="Memories & Goodbyes" id="memories">
       <button type="button" className="btn btn-primary" onClick={handleClick}>Add your memory</button>
       <hr />
+      {comments.length < 1 && (<h3 className="text-center memories__zero-state-title">No memories yet</h3>)}
       <div className="card-columns">
         {comments.map((comment) => (
           <div className="card">
@@ -75,7 +131,7 @@ const Memories = () => {
 
       </div>
 
-      <MemoryFormModal onDismiss={handleDismiss} show={showForm} />
+      <MemoryFormModal onSuccess={fetchComments} onDismiss={handleDismiss} show={showForm} />
     </Section>
   );
 };
