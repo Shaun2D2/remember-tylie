edfcc769ea4fe445790742edd9a03b77d0407229
Shaun Lobsinger <shaun@jolobo.com>
2020-12-05 18:10:18 -0700

    updated navigation click.

--- src/react/components/Navigation.jsx
+++ src/react/components/Navigation.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
 const { Brand, Toggle, Collapse } = Navbar;
 const { Link } = Nav;
 
-const Navigation = () => (
-  <Navbar bg="dark" variant="dark" expand="lg">
-    <Brand href="#home">Tylie Sanders</Brand>
-    <Toggle aria-controls="basic-navbar-nav" />
-    <Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Link href="#eulogy">Eulogy</Link>
-        <Link href="#photos">Photos</Link>
-        <Link href="#memories">Memories</Link>
-      </Nav>
-    </Collapse>
-  </Navbar>
-);
+const Navigation = () => {
+  const handleClick = () => {
+    window.open('./tylie-memorial.pdf');
+  };
+
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Brand href="#home">Tylie Sanders</Brand>
+      <Toggle aria-controls="basic-navbar-nav" />
+      <Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Link href="#eulogy">Eulogy</Link>
+          <Link href="#photos">Photos</Link>
+          <Link href="#memories">Memories</Link>
+        </Nav>
+        <Nav inline>
+          <Button onClick={handleClick}>Memorial</Button>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  )
+};
 
 export default Navigation;
