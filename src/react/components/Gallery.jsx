7dc797a78d5d1193421df02c4d0d8d4f0946767f
Shaun Lobsinger <shaun@jolobo.com>
2020-12-05 20:13:43 -0700

    updated photos and gallery

--- src/react/components/Gallery.jsx
+++ src/react/components/Gallery.jsx
@@ -4,33 +4,47 @@ import Gallery from 'react-photo-gallery';
 
 import Section from './Section';
 
-const photos = [
+const PHOTOS = [
   {
     src: '/tylie-1.jpg',
-    width: 1,
-    height: 1,
+    width: 1.5,
+    height: 2,
   },
   {
     src: '/tylie-2.jpg',
-    width: 1,
-    height: 1,
+    width: 1.5,
+    height: 2,
   },
   {
     src: '/tylie-4.jpg',
-    width: 1,
-    height: 1,
+    width: 1.5,
+    height: 2,
   },
   {
     src: '/tylie-3.jpg',
     width: 5,
     height: 4,
   },
-
+  {
+    src: '/tylie-5.jpg',
+    width: 1,
+    height: 2,
+  },
+  {
+    src: '/tylie-6.jpg',
+    width: 10,
+    height: 6,
+  },
+  {
+    src: '/order-of-service.jpg',
+    width: 8,
+    height: 8,
+  },
 ];
 
 const SiteGallery = () => (
   <Section title="Photos" id="photos">
-    <Gallery photos={photos} direction="column" />
+    <Gallery photos={PHOTOS} direction="row" />
   </Section>
 );
 
